4e8178bc449a8c9d61b379ab29791db97e598c3f
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-24 17:00:30 +0200

    Pierwsza wersja

---
+++ src/Calculator.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Podaj liczbę:");
+//        Scanner scanner = new Scanner(System.in);
+//        int liczba1 = scanner.nextInt();
+//
+//        String operacja = scanner.next();
+//
+//        int liczba2 = scanner.nextInt();
+
+
+        testuj(2, "+", 2, 4);
+        testuj(2, "-", 2, 0);
+        testuj(3, "*", 5, 15);
+        testuj(3, "/", 2, 1);
+        testuj(3, "%", 2, 1);
+        testuj(11, "%", 5, 1);
+    }
+
+    private static void testuj(int liczba1, String operacja, int liczba2, int spodziewanyWynik) {
+        int wynik = policz(liczba1, liczba2, operacja);
+        if (spodziewanyWynik != wynik) {
+            System.out.println("UWAGA BŁĄD!");
+        }
+        System.out.println("liczba1: " + liczba1 +
+                " liczba2: " + liczba2 +
+                " operacja: " + operacja +
+                " spodziewany wynik: " + spodziewanyWynik +
+                " prawdziwy wynik: " + wynik);
+
+
+    }
+
+
+    private static int policz(int liczba1, int liczba2, String operacja) {
+        if (operacja.equals("+")) {
+            return liczba1 + liczba2;
+        }
+
+        if (operacja.equals("/")) {
+            return liczba1 / liczba2;
+        }
+
+        if (operacja.equals("*")) {
+            return liczba1 * liczba2;
+        }
+
+        if (operacja.equals("-")) {
+            return liczba1 - liczba2;
+        }
+
+        if (operacja.equals("%")) {
+            return liczba1 % liczba2;
+        }
+
+        return -1;
+    }
+}
