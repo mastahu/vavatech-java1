ac0428800aec52d33e5edd0fc1cad2eb852ac3fe
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-24 17:01:05 +0200

    Pętle

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 7;
+        do {
+            System.out.println("w pętli: " + i);
+            i++;
+        } while(i <= 5);
+        System.out.println("za pętlą: " + i);
+    }
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int i = 7;
+        while (i <= 5) {
+            System.out.println("w pętli: " + i);
+            i++;
+        }
+        System.out.println("za pętlą: " + i);
+    }
+
+}
