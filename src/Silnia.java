2bd831785846498fa9de498e5ce393d43773cf86
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-25 10:32:28 +0200

    zmiany

---
+++ src/Silnia.java
@@ -0,0 +1,21 @@
+
+// Super klasa
+public class Silnia {
+
+    public static int silniaIteracyjnie(int liczba) {
+        int wynik = 1;
+        for (int i = 2; i <= liczba; i++) {
+            wynik = i * wynik;
+        }
+        return wynik;
+    }
+
+    public static int silniaRekurencyjnie(int liczba) {
+        if (liczba == 1) {
+            return 1;
+        }
+
+        return liczba * silniaRekurencyjnie(liczba - 1);
+    }
+
+}
