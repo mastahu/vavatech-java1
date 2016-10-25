4a7a9da2ab3cebfa9d598142f5415391f0fa5bc1
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-25 12:15:15 +0200

    ćwiczenie 7

---
+++ src/Cwiczenie7.java
@@ -0,0 +1,21 @@
+public class Cwiczenie7 {
+
+    public static void main(String[] args) {
+        byte a = 127;
+        a += 1;
+        System.out.println(a);
+
+        short s = -32000; s -= 1000;
+        System.out.println(s);
+
+        int i = 2000000000; i = 2 * i;
+        System.out.println(i);
+
+        char c = 50; System.out.println(c);
+        char literkaA = 65; System.out.println(literkaA);
+        char literkaEU = 322; System.out.println(literkaEU);
+
+        int j = 'A'; System.out.println(j);
+
+    }
+}
